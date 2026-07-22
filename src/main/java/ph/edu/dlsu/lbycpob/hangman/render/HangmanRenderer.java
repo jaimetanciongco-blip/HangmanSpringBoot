8bc857ec5c61d2bf76f5fd1312552dd4cd8afb34
jaimetanciongco-blip <jaime_tanciongco@dlsu.edu.ph>
2026-07-22 21:30:13 +0800

    added codes to HangmanRenderer

--- src/main/java/ph/edu/dlsu/lbycpob/hangman/render/HangmanRenderer.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangman/render/HangmanRenderer.java
@@ -1,4 +1,8 @@
 package ph.edu.dlsu.lbycpob.hangman.render;
 
+import java.io.IOException;
+import java.util.List;
+
 public interface HangmanRenderer {
+    List<String> render(int guessesRemaining) throws IOException;
 }
